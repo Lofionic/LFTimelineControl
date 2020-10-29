8da92d62f3ec19b6b786e578c977b7bc7596c4b5
Chris Rivers <chris@lofionic.co.uk>
2020-10-29 13:11:07 +0000

    Add label and reset button to example app

--- Example/LFTimelineControl/ViewController.swift
+++ Example/LFTimelineControl/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
+import LFTimelineControl
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timelineControl: TimelineControl!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        timelineControl.addTarget(self, action: #selector(updateLabel), for: .valueChanged)
+        updateLabel()
     }
-
-
+    
+    @IBAction func tapReset(_ sender: Any) {
+        timelineControl.setLocation(0, animated: true)
+        updateLabel()
+    }
+    
+    @objc
+    private func updateLabel() {
+        label.text = "\(String(format: "%.02f", timelineControl.location))"
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
 }
 
